4b377fa5c382f076573893593af1301ee17d98b3
melissa-24 <melissalongenberger24@gmail.com>
2022-06-10 22:12:52 -0400

    saving root changes

---
+++ app/views/main.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from app.models import *
+from app.general import *
+
+
+def index(request):
+    return render(request, 'index.html')
