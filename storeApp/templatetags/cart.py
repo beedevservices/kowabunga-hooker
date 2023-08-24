389d0a3ece0ebe5b2bca5db2c95d061221ce308e
Melissa Longenberger <melissalongenberger24@gmail.com>
2023-08-24 05:47:42 -0400

    cart set up done mostly

--- storeApp/templatetags/cart.py
+++ storeApp/templatetags/cart.py
@@ -16,7 +16,35 @@ def inTheCart(prod, cart):
 @register.filter(name='prodQuantity')
 def prodQuantity(prod, cart):
     keys = cart.keys()
+    # print(keys)
     for row in keys:
         if int(row) == prod:
+            print('quantity filter', cart.get(row))
             return cart.get(row)
     return 0
+
+@register.filter(name='noPrice')
+def noPrice(prod):
+    if not prod.price:
+        return True
+    else:
+        return False
+    
+@register.filter(name='prodTotal')
+def prodTotal(prod, cart):
+    return prod.price * prodQuantity(prod, cart)
+
+@register.filter(name='cartTotal')
+def cartTotal(prods, cart):
+    sum = 0
+    count = 0
+    for prod in prods:
+        if not prod.price:
+            count += 1
+        else:
+            sum += prodTotal(prod, cart)
+    if count > 0:
+        theTotal = f'${sum} + Custom Items + Shipping'
+    else:
+        theTotal = f'${sum} + Shipping'
+    return theTotal
--- storeApp/views/cart.py
+++ storeApp/views/cart.py
@@ -9,3 +9,15 @@ def cart(request):
         url = '/cart/'
         request.session['url'] = url
         return redirect('/logReg/')
+    else:
+        user = User.objects.get(id=request.session['user_id'])
+        cart = request.session['cart']
+        ids = list(request.session.get('cart').keys())
+        products = Product.objects.filter(id__in=ids)
+        print(products, cart)
+        context = {
+            'user': user,
+            'cart': cart,
+            'products': products,
+        }
+        return render(request, 'cart.html', context)
